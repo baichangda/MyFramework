2e329e419126e98c782cd64edda99a41d8d343b1
baichangda <471267877@qq.com>
2025-01-24 13:26:03 +0800

    c

---
+++ Server-BusinessProcess-Backend/src/main/java/cn/bcd/businessProcess/backend/base/support_kafka/example/consumer/Consumer.java
@@ -0,0 +1,18 @@
+package cn.bcd.businessProcess.backend.base.support_kafka.example.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
+
+//@Service
+public class Consumer {
+    Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+    @KafkaListener(topics = "test")
+    public void processMessage(ConsumerRecord<byte[], byte[]> consumerRecord) {
+        logger.info("receive message key[{}] value[{}]", new String(consumerRecord.key()), new String(consumerRecord.value()));
+    }
+
+}
