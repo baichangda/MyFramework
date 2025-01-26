d8d3cacd1dfa33286c4784fcfcbc881cdfcdff31
baichangda <471267877@qq.com>
2025-01-26 11:40:17 +0800

    c

---
+++ Server-DataProcess-Gateway-Tcp/src/main/java/cn/bcd/dataProcess/gateway/tcp/gb32960/Monitor_gb32960.java
@@ -0,0 +1,24 @@
+package cn.bcd.dataProcess.gateway.tcp.gb32960;
+
+import cn.bcd.base.util.StringUtil;
+import cn.bcd.dataProcess.gateway.tcp.Monitor;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.LongAdder;
+
+@Component
+public class Monitor_gb32960 implements Monitor {
+    public final static LongAdder blockingNum = new LongAdder();
+    public final static LongAdder receiveNum = new LongAdder();
+    public final static LongAdder sendKafkaNum = new LongAdder();
+
+    @Override
+    public String log(Duration period) {
+        return StringUtil.format("gb32960 clientNum[{}] blocking[{}] receiveSpeed[{}/s] sendKafkaSpeed[{}/s]",
+                Session_gb32960.getSessionMap().size(),
+                blockingNum.intValue(),
+                Monitor.formatSpeed(receiveNum.sumThenReset(), period),
+                Monitor.formatSpeed(sendKafkaNum.sumThenReset(), period));
+    }
+}
