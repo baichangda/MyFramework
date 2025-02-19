5cbf0c19650f1db5406bc6c6ec0b517080b0acfa
baichangda <bcd@5221527>
2025-02-19 22:16:36 +0800

    c

--- Lib-Base-Executor/src/main/java/cn/bcd/lib/base/executor/SingleThreadExecutor2.java
+++ Lib-Base-Executor/src/main/java/cn/bcd/lib/base/executor/SingleThreadExecutor2.java
@@ -17,144 +17,132 @@ public class SingleThreadExecutor2 {
     public final String threadName;
     public final int queueSize;
     public final boolean schedule;
-    public final BlockingQueue<Runnable> blockingQueue;
     public final BlockingChecker blockingChecker;
+
+    public BlockingQueue<Runnable> blockingQueue;
     //用于通知其他线程退出
-    public final CountDownLatch quitNotifier = new CountDownLatch(1);
+    public CountDownLatch quitNotifier;
 
     /**
      * 任务执行器
      */
-    public final ThreadPoolExecutor executor;
+    public ThreadPoolExecutor executor;
     /**
      * 计划任务执行器
      */
-    public final ScheduledThreadPoolExecutor executor_schedule;
+    public ScheduledThreadPoolExecutor executor_schedule;
     /**
      * 阻塞检查器
      */
-    public final ScheduledThreadPoolExecutor executor_blockingChecker;
+    public ScheduledThreadPoolExecutor executor_blockingChecker;
 
     /**
      * 当前执行器绑定的线程
      */
-    public final Thread thread;
+    public Thread thread;
 
-    /**
-     * 0、正在运行
-     * 1、停止中
-     * 2、已停止
-     */
-    volatile int status = 0;
+    volatile boolean running;
 
-    public final Consumer<SingleThreadExecutor2> doBeforeShutdown;
 
-    public SingleThreadExecutor2(String threadName, int queueSize, boolean schedule, BlockingChecker blockingChecker, Consumer<SingleThreadExecutor2> doBeforeShutdown) {
+    public SingleThreadExecutor2(String threadName,
+                                 int queueSize,
+                                 boolean schedule,
+                                 BlockingChecker blockingChecker) {
         this.threadName = threadName;
         this.queueSize = queueSize;
         this.schedule = schedule;
         this.blockingChecker = blockingChecker;
-        this.doBeforeShutdown = doBeforeShutdown;
-        if (queueSize == 0) {
-            this.blockingQueue = new MpscUnboundArrayBlockingQueue<>(1024, WaitStrategy.PROGRESSIVE);
-        } else {
-            this.blockingQueue = new MpscArrayBlockingQueue<>(queueSize, WaitStrategy.PROGRESSIVE);
-        }
-        executor = new ThreadPoolExecutor(
-                1,
-                1,
-                0,
-                TimeUnit.SECONDS,
-                this.blockingQueue,
-                r -> new Thread(r, threadName),
-                (r, executor) -> {
-                    if (!executor.isShutdown()) {
-                        try {
+
+    }
+
+    public synchronized void init() {
+        if (!running) {
+            running = true;
+            quitNotifier = new CountDownLatch(1);
+            if (queueSize == 0) {
+                this.blockingQueue = new MpscUnboundArrayBlockingQueue<>(1024, WaitStrategy.PROGRESSIVE);
+            } else {
+                this.blockingQueue = new MpscArrayBlockingQueue<>(queueSize, WaitStrategy.PROGRESSIVE);
+            }
+            executor = new ThreadPoolExecutor(
+                    1,
+                    1,
+                    0,
+                    TimeUnit.SECONDS,
+                    this.blockingQueue,
+                    r -> new Thread(r, threadName),
+                    (r, executor) -> {
+                        if (!executor.isShutdown()) {
+                            try {
 //                    logger.warn("workThread[{}] RejectedExecutionHandler",threadName);
-                            executor.getQueue().put(r);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
+                                executor.getQueue().put(r);
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
                         }
-                    }
-                });
+                    });
 
-        //获取当前线程
-        try {
-            thread = executor.submit(Thread::currentThread).get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw BaseException.get(e);
-        }
+            //获取当前线程
+            try {
+                thread = executor.submit(Thread::currentThread).get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw BaseException.get(e);
+            }
 
-        if (schedule) {
-            executor_schedule = new ScheduledThreadPoolExecutor(1, r -> new Thread(r, threadName + "-schedule"));
-        } else {
-            executor_schedule = null;
-        }
+            if (schedule) {
+                executor_schedule = new ScheduledThreadPoolExecutor(1, r -> new Thread(r, threadName + "-schedule"));
+            } else {
+                executor_schedule = null;
+            }
 
-        if (blockingChecker != null) {
-            //开启阻塞监控
-            int maxBlockingTimeInSecond = blockingChecker.maxBlockingTimeInSecond;
-            int periodInSecond = blockingChecker.periodInSecond;
-            this.executor_blockingChecker = new ScheduledThreadPoolExecutor(1, r -> new Thread(r, threadName + "-blockingChecker"));
-            this.executor_blockingChecker.scheduleWithFixedDelay(() -> {
-                long start = DateUtil.CacheSecond.current();
-                Future<?> future = submit(() -> {
-                });
-                try {
-                    boolean quit = quitNotifier.await(maxBlockingTimeInSecond, TimeUnit.SECONDS);
-                    if (quit) {
-                        return;
-                    }
-                    TimeUnit.SECONDS.sleep(maxBlockingTimeInSecond);
-                    while (!future.isDone()) {
-                        long blockingSecond = DateUtil.CacheSecond.current() - start;
-                        if (blockingSecond >= maxBlockingTimeInSecond) {
-                            logger.warn("WorkExecutor blocking threadName[{}] blockingTime[{}s>={}s] queueSize[{}]", threadName, blockingSecond, maxBlockingTimeInSecond, executor.getQueue().size());
-                        }
-                        quit = quitNotifier.await(blockingChecker.periodWhenBlockingInSecond, TimeUnit.SECONDS);
+            if (blockingChecker != null) {
+                //开启阻塞监控
+                int maxBlockingTimeInSecond = blockingChecker.maxBlockingTimeInSecond;
+                int periodInSecond = blockingChecker.periodInSecond;
+                this.executor_blockingChecker = new ScheduledThreadPoolExecutor(1, r -> new Thread(r, threadName + "-blockingChecker"));
+                this.executor_blockingChecker.scheduleWithFixedDelay(() -> {
+                    long start = DateUtil.CacheSecond.current();
+                    Future<?> future = submit(() -> {
+                    });
+                    try {
+                        boolean quit = quitNotifier.await(maxBlockingTimeInSecond, TimeUnit.SECONDS);
                         if (quit) {
                             return;
                         }
+                        TimeUnit.SECONDS.sleep(maxBlockingTimeInSecond);
+                        while (!future.isDone()) {
+                            long blockingSecond = DateUtil.CacheSecond.current() - start;
+                            if (blockingSecond >= maxBlockingTimeInSecond) {
+                                logger.warn("WorkExecutor blocking threadName[{}] blockingTime[{}s>={}s] queueSize[{}]", threadName, blockingSecond, maxBlockingTimeInSecond, executor.getQueue().size());
+                            }
+                            quit = quitNotifier.await(blockingChecker.periodWhenBlockingInSecond, TimeUnit.SECONDS);
+                            if (quit) {
+                                return;
+                            }
+                        }
+                    } catch (InterruptedException ex) {
+                        throw BaseException.get(ex);
                     }
-                } catch (InterruptedException ex) {
-                    throw BaseException.get(ex);
-                }
-            }, periodInSecond, periodInSecond, TimeUnit.SECONDS);
-        } else {
-            this.executor_blockingChecker = null;
-        }
-    }
-
-    private void doBeforeShutdown() {
-        if (doBeforeShutdown != null) {
-            //回调
-            SingleThreadExecutor2 temp = this;
-            if (inThread()) {
-                doBeforeShutdown.accept(temp);
+                }, periodInSecond, periodInSecond, TimeUnit.SECONDS);
             } else {
-                try {
-                    executor.submit(() -> {
-                        doBeforeShutdown.accept(temp);
-                    }).get();
-                } catch (InterruptedException | ExecutionException ex) {
-                    logger.error("error", ex);
-                }
+                this.executor_blockingChecker = null;
             }
         }
     }
 
-    public void shutdown() {
-        if (status == 0) {
-            synchronized (this) {
-                if (status == 0) {
-                    status = 1;
-                    doBeforeShutdown();
-                    //通知退出
-                    quitNotifier.countDown();
-                    ExecutorUtil.shutdownAllThenAwait(executor, executor_schedule, executor_blockingChecker);
-                    status = 2;
-                }
-            }
+    public synchronized void destroy() {
+        if (running) {
+            running = false;
+            //通知退出
+            quitNotifier.countDown();
+            ExecutorUtil.shutdownAllThenAwait(executor, executor_schedule, executor_blockingChecker);
+            //清空变量
+            blockingQueue = null;
+            quitNotifier = null;
+            executor = null;
+            executor_schedule = null;
+            executor_blockingChecker = null;
+            thread = null;
         }
     }
 
@@ -163,8 +151,8 @@ public class SingleThreadExecutor2 {
     }
 
     private void checkRunning() {
-        if (status > 0) {
-            throw BaseException.get("executor[{}] status[{}]", threadName, status);
+        if (running) {
+            throw BaseException.get("executor[{}] not running", threadName);
         }
     }
 
