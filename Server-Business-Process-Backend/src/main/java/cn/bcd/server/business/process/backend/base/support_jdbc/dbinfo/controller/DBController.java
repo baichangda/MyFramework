c49fbb4f7e022db5bf865bafcb84581c8559e734
baichangda <471267877@qq.com>
2025-03-10 14:07:31 +0800

    c

--- Server-Business-Process-Backend/src/main/java/cn/bcd/server/business/process/backend/base/support_jdbc/dbinfo/controller/DBController.java
+++ Server-Business-Process-Backend/src/main/java/cn/bcd/server/business/process/backend/base/support_jdbc/dbinfo/controller/DBController.java
@@ -32,9 +32,7 @@ public class DBController extends BaseController {
     @RequestMapping(value = "/exportSpringDBDesignerExcel", method = RequestMethod.GET)
     @Operation(summary = "导出spring数据库设计")
     @ApiResponse(responseCode = "200", description = "导出结果")
-    public void exportSpringDBDesignerExcel(
-            @Parameter(description = "数据库名称、默认为spring配置数据库") @RequestParam(required = false) String dbName,
-            HttpServletResponse response) {
+    public void exportSpringDBDesignerExcel(@Parameter(description = "数据库名称、默认为spring配置数据库") @RequestParam(required = false) String dbName, HttpServletResponse response) {
         try {
             dbService.exportSpringDBDesignerExcel(dbName, response.getOutputStream(), () -> {
                 String fileName = "db-" + dbName + ".xlsx";
@@ -48,12 +46,7 @@ public class DBController extends BaseController {
     @RequestMapping(value = "/exportDBDesignerExcel", method = RequestMethod.GET)
     @Operation(summary = "导出数据库设计")
     @ApiResponse(responseCode = "200", description = "导出结果")
-    public void exportDBDesignerExcel(
-            @Parameter(description = "数据库url(例如:127.0.0.1:3306)") @RequestParam String url,
-            @Parameter(description = "数据库用户名") @RequestParam String username,
-            @Parameter(description = "数据库密码") @RequestParam String password,
-            @Parameter(description = "数据库名称") @RequestParam String dbName,
-            HttpServletResponse response) {
+    public void exportDBDesignerExcel(@Parameter(description = "数据库url(例如:127.0.0.1:3306)") @RequestParam String url, @Parameter(description = "数据库用户名") @RequestParam String username, @Parameter(description = "数据库密码") @RequestParam String password, @Parameter(description = "数据库名称") @RequestParam String dbName, HttpServletResponse response) {
         try {
             dbService.exportDBDesignerExcel(url, username, password, dbName, response.getOutputStream(), () -> {
                 String fileName = "db-" + dbName + ".xlsx";
@@ -65,14 +58,12 @@ public class DBController extends BaseController {
     }
 
     @RequestMapping(value = "/test", method = RequestMethod.POST)
-    @Operation(summary = "导出spring数据库设计")
+    @Operation(summary = "测试spring导出bug")
     @ApiResponse(responseCode = "200", description = "导出结果")
-    public void test(
-            @Parameter(description = "文件") @RequestParam MultipartFile file,
-            HttpServletResponse response) {
+    public void testExportBug(@Parameter(description = "文件") @RequestParam MultipartFile file, HttpServletResponse response) {
         try {
             dbService.exportSpringDBDesignerExcel("bcd", response.getOutputStream(), () -> {
-                String fileName = "db-" + "bcd" + ".xlsx";
+                String fileName = "db-bcd.xlsx";
                 doBeforeResponseFile(fileName, response);
             });
         } catch (IOException e) {
