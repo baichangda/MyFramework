c835dda2639375fd228f005d0ac8443816139cc2
baichangda <471267877@qq.com>
2025-03-27 16:19:28 +0800

    c

---
+++ Lib-Data-Init/src/main/java/cn/bcd/lib/data/init/util/OkHttpUtil.java
@@ -0,0 +1,32 @@
+package cn.bcd.lib.data.init.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+
+public class OkHttpUtil {
+
+    static Logger logger = LoggerFactory.getLogger(OkHttpUtil.class);
+
+    public final static OkHttpClient client = new OkHttpClient.Builder()
+            .addInterceptor(newHttpLoggingInterceptor())
+            .connectTimeout(Duration.ofSeconds(10))
+            .readTimeout(Duration.ofSeconds(30))
+            .writeTimeout(Duration.ofSeconds(30))
+            .build();
+
+    private static HttpLoggingInterceptor newHttpLoggingInterceptor() {
+        final HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor(s -> {
+            try {
+                logger.info(s);
+            } catch (Exception ex) {
+                logger.error("error", ex);
+            }
+        });
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return httpLoggingInterceptor;
+    }
+}
