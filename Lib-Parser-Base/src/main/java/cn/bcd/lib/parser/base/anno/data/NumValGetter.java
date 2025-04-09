88638b1104e3dd9fb00c05bc351165f71f86791c
baichangda <471267877@qq.com>
2025-04-09 10:30:55 +0800

    c

--- Lib-Parser-Base/src/main/java/cn/bcd/lib/parser/base/anno/data/NumValGetter.java
+++ Lib-Parser-Base/src/main/java/cn/bcd/lib/parser/base/anno/data/NumValGetter.java
@@ -37,37 +37,11 @@ public abstract class NumValGetter {
     public abstract long getVal_long(NumType numType, int type);
 
 
-    public byte getVal(NumType numType, NumVal_byte numVal) {
-        if (numVal.type() == 0) {
-            return numVal.val();
-        } else {
-            return (byte) getVal_int(numType, numVal.type());
-        }
-    }
-
-    public short getVal(NumType numType, NumVal_short numVal) {
-        if (numVal.type() == 0) {
-            return numVal.val();
-        } else {
-            return (short) getVal_int(numType, numVal.type());
-        }
-    }
-
-    public int getVal(NumType numType, NumVal_int numVal) {
-        if (numVal.type() == 0) {
-            return numVal.val();
-        } else {
-            return getVal_int(numType, numVal.type());
-        }
-    }
-
-    public long getVal(NumType numType, NumVal_long numVal) {
-        if (numVal.type() == 0) {
-            return numVal.val();
-        } else {
-            return getVal_long(numType, numVal.type());
-        }
-    }
+    /**
+     * --------------------------------------------------
+     * 以下方法用于简单值的解析和反解析
+     * 所谓简单值指的是只需要判断原始值是否异常等等情况、而不用进行偏移量运算
+     */
 
     public NumVal_byte getNumVal_byte(NumType numType, byte val) {
         int type = getType(numType, val);
@@ -102,6 +76,39 @@ public abstract class NumValGetter {
             return new NumVal_long(0, val);
         } else {
             return new NumVal_long(type, 0L);
+        }
+    }
+
+
+    public byte getVal(NumType numType, NumVal_byte numVal) {
+        if (numVal.type() == 0) {
+            return numVal.val();
+        } else {
+            return (byte) getVal_int(numType, numVal.type());
+        }
+    }
+
+    public short getVal(NumType numType, NumVal_short numVal) {
+        if (numVal.type() == 0) {
+            return numVal.val();
+        } else {
+            return (short) getVal_int(numType, numVal.type());
+        }
+    }
+
+    public int getVal(NumType numType, NumVal_int numVal) {
+        if (numVal.type() == 0) {
+            return numVal.val();
+        } else {
+            return getVal_int(numType, numVal.type());
+        }
+    }
+
+    public long getVal(NumType numType, NumVal_long numVal) {
+        if (numVal.type() == 0) {
+            return numVal.val();
+        } else {
+            return getVal_long(numType, numVal.type());
         }
     }
 }
