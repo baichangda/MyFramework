18ada7f95167194f9b191e2d532ba7e94f1b9b82
baichangda <471267877@qq.com>
2025-06-11 09:43:55 +0800

    c

--- Lib-Parser-Protocol-GB32960/src/main/java/cn/bcd/lib/parser/protocol/gb32960/v2016/data/ParamQueryRequest.java
+++ Lib-Parser-Protocol-GB32960/src/main/java/cn/bcd/lib/parser/protocol/gb32960/v2016/data/ParamQueryRequest.java
@@ -4,6 +4,7 @@ import cn.bcd.lib.parser.base.anno.F_date_bytes_6;
 import cn.bcd.lib.parser.base.anno.F_num;
 import cn.bcd.lib.parser.base.anno.F_num_array;
 import cn.bcd.lib.parser.base.data.NumType;
+import cn.bcd.lib.parser.base.data.NumVal_short;
 
 import java.util.Date;
 
@@ -11,7 +12,7 @@ public class ParamQueryRequest implements PacketData{
     @F_date_bytes_6
     public Date queryTime;
     @F_num(type = NumType.uint8, var = 'n')
-    public short num;
+    public NumVal_short num;
     @F_num_array(singleType = NumType.uint8, lenExpr = "n")
     public short[] paramIds;
 }
