7646e03ee4c830b768b18b576193b7cc862a45a4
baichangda <471267877@qq.com>
2025-06-18 15:36:04 +0800

    c

--- Lib-Base/src/main/java/cn/bcd/lib/base/util/StringUtil.java
+++ Lib-Base/src/main/java/cn/bcd/lib/base/util/StringUtil.java
@@ -1,6 +1,5 @@
 package cn.bcd.lib.base.util;
 
-import com.google.common.base.Strings;
 import org.slf4j.helpers.MessageFormatter;
 
 import java.util.List;
@@ -97,116 +96,9 @@ public class StringUtil {
                 || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION; // 通用标点
     }
 
-    /**
-     * 根据给定的表名、行列值、生成控制台类似表格样式的字符串、便于打印输出
-     * 字符串仅支持ascii字符
-     *
-     * @param name
-     * @param list
-     * @return
-     */
-    public static String tableFormat(String name, List<List<String>> list) {
-        StringBuilder res = new StringBuilder();
-        boolean noData = list == null || list.isEmpty() || list.getFirst().isEmpty();
-        if (name == null && noData) {
-            return "";
-        }
-        if (noData) {
-            String data = "empty";
-            int nameLength = name.length();
-            int dataLength = data.length();
-            int len = Math.max(nameLength, dataLength) + 4;
-            int width = len - 2;
-            int leftSpace = (width - nameLength) / 2;
-            int rightSpace = width - nameLength - leftSpace;
-            int dataLeftSpace = (width - dataLength) / 2;
-            int dataRightSpace = width - dataLength - dataLeftSpace;
-            res.append("+").append(Strings.repeat("-", width)).append("+\n");
-            res.append("|").append(Strings.repeat(" ", leftSpace)).append(name).append(Strings.repeat(" ", rightSpace)).append("|\n");
-            res.append("+").append(Strings.repeat("-", width)).append("+\n");
-            res.append("|").append(Strings.repeat(" ", dataLeftSpace)).append(data).append(Strings.repeat(" ", dataRightSpace)).append("|\n");
-            res.append("+").append(Strings.repeat("-", width)).append("+\n");
-            return res.toString();
-        }
-        //计算列宽
-        int[] colWidth = new int[list.getFirst().size()];
-        for (List<String> row : list) {
-            for (int i = 0; i < row.size(); i++) {
-                String ele = row.get(i);
-                colWidth[i] = Math.max(colWidth[i], ele.length() + 2);
-            }
-        }
-
-        if (name != null) {
-            int len = colWidth.length + 1;
-            for (int i : colWidth) {
-                len += i;
-            }
-            int width = len - 2;
-            //根据name调整colWidth
-            if (name.length() + 2 > width) {
-                len = colWidth.length + 1;
-                int diff = name.length() + 2 - width;
-                int div = diff / colWidth.length;
-                int leave = diff % colWidth.length;
-                for (int i = 0; i < colWidth.length; i++) {
-                    colWidth[i] += div;
-                    if (i < leave) {
-                        colWidth[i]++;
-                    }
-                    len += colWidth[i];
-                }
-                width = len - 2;
-            }
-
-            int leftSpace = (width - name.length()) / 2;
-            int rightSpace = width - name.length() - leftSpace;
-            //添加表头
-            res.append("+").append(Strings.repeat("-", width)).append("+\n");
-            res.append("|").append(Strings.repeat(" ", leftSpace)).append(name).append(Strings.repeat(" ", rightSpace)).append("|\n");
-        }
-
-        StringBuilder rowLine = new StringBuilder();
-        for (int i : colWidth) {
-            rowLine.append("+");
-            rowLine.append(Strings.repeat("-", i));
-        }
-        rowLine.append("+\n");
-        for (List<String> row : list) {
-            res.append(rowLine);
-            StringBuilder rowSb = new StringBuilder();
-            for (int i = 0; i < row.size(); i++) {
-                rowSb.append("|");
-                int width = colWidth[i];
-                String ele = row.get(i);
-                //计算当前值前后间距、如果前后间距不等、则左间距为奇数
-                int leftSpace = (width - ele.length()) / 2;
-                int rightSpace = width - ele.length() - leftSpace;
-                //拼接字符串
-                String colSb = Strings.repeat(" ", leftSpace) +
-                        ele +
-                        Strings.repeat(" ", rightSpace);
-                rowSb.append(colSb);
-            }
-            rowSb.append("|\n");
-            res.append(rowSb);
-        }
-        res.append(rowLine);
-        return res.toString();
-    }
-
     public static void main(String[] args) {
         System.out.println(format("{}-{}", 123, null));
         System.out.println(format("{}-\\{}-{}", 123, null, "abc"));
-
-        String s = tableFormat("a", List.of(
-//                List.of("1", "2", "3"),
-//                List.of("122223", "456", "789"),
-//                List.of("abc", "d5555555555ef", "ghi"),
-//                List.of("jkl", "mno", "pqr"),
-//                List.of("abc", "d5555555555ef", "ghi")
-        ));
-        System.out.println(s);
     }
 
 
