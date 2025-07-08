650e742171638da467be34e2a5392469cef776d9
baichangda <471267877@qq.com>
2025-07-08 15:19:55 +0800

    c

--- Server-Business-Process-Backend/src/main/java/cn/bcd/server/business/process/backend/base/support_task/StopResult.java
+++ Server-Business-Process-Backend/src/main/java/cn/bcd/server/business/process/backend/base/support_task/StopResult.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum StopResult {
-    CANCEL_SUCCEED(0, "任务取消成功"),
+    CANCEL_SUCCEED(0, "任务待执行、取消成功"),
     WAIT_OR_IN_EXECUTING_NOT_FOUND(1, "找不到待执行或执行中的任务"),
     IN_EXECUTING_INTERRUPT_SUCCEED(2, "任务执行中、已请求打断、等待停止"),
     ;
