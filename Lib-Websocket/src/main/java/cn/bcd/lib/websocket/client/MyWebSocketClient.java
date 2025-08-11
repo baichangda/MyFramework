10a15b854738f60eb7ec1548cb8875f72204ebed
baichangda <471267877@qq.com>
2025-08-11 10:14:43 +0800

    c

--- Lib-Websocket/src/main/java/cn/bcd/lib/websocket/client/MyWebSocketClient.java
+++ Lib-Websocket/src/main/java/cn/bcd/lib/websocket/client/MyWebSocketClient.java
@@ -9,11 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class MyWebSocketClient {
 
@@ -21,30 +20,36 @@ public class MyWebSocketClient {
 
     static final ScheduledExecutorService pool = Executors.newSingleThreadScheduledExecutor();
 
+    public final String host;
+    public final int port;
     public final String url;
     public final Duration autoReconnectPeriod;
     private final WebSocketClient webSocketClient;
     private final Handler<Void> closeHandler;
     private final Handler<String> textMessageHandler;
-    private final AtomicReference<WebSocket> webSocketHolder = new AtomicReference<>();
-    private final AtomicLong nextConnectTs = new AtomicLong();
+    private volatile WebSocket webSocket;
+    private long nextConnectTs;
 
-    public MyWebSocketClient(String url, Duration autoReconnectPeriod, Handler<String> textMessageHandler) {
-        this.url = url;
+    public MyWebSocketClient(String host, int port, Duration autoReconnectPeriod, Handler<String> textMessageHandler) {
+        this.host = host;
+        this.port = port;
+        this.url = host + ":" + port;
         this.textMessageHandler = textMessageHandler;
         closeHandler = v -> {
-            logger.info("on close");
-            webSocketHolder.set(null);
-            nextConnectTs.set(0);
-            connect();
+            pool.execute(() -> {
+                logger.info("on close");
+                webSocket = null;
+                nextConnectTs = 0;
+                connect();
+            });
         };
         Vertx vertx = Vertx.builder().build();
         webSocketClient = vertx.createWebSocketClient();
         this.autoReconnectPeriod = autoReconnectPeriod;
     }
 
-    public boolean connected() {
-        return webSocketHolder.get() != null;
+    public final boolean connected() {
+        return webSocket != null;
     }
 
     /**
@@ -55,35 +60,43 @@ public class MyWebSocketClient {
      * @param text
      * @return
      */
-    public Future<Void> sendText(String text) {
-        WebSocket webSocket = webSocketHolder.get();
+    public final Future<Void> sendText(String text) {
+        WebSocket webSocket = this.webSocket;
         if (webSocket == null) {
             return null;
         }
         return webSocket.writeTextMessage(text);
     }
 
+    public final CompletableFuture<Void> connect() {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        pool.execute(() -> {
+            connectInterval(future);
+        });
+        return future;
+    }
 
-    public void connect() {
+    private void connectInterval(CompletableFuture<Void> future) {
         long ts = System.currentTimeMillis();
-        long nextTs = nextConnectTs.get();
+        long nextTs = nextConnectTs;
         long periodMs = autoReconnectPeriod.toMillis();
         if (nextTs == 0) {
             nextTs = ts;
         }
         long waitTs = nextTs - ts;
-        nextConnectTs.set(nextTs + periodMs);
+        nextConnectTs = nextTs + periodMs;
         pool.schedule(() -> {
             logger.info("connect ws[{}]", url);
             webSocketClient.connect(8080, "127.0.0.1", "")
                     .onSuccess(w -> {
-                        logger.error("connect url[{}] succeed", url);
-                        webSocketHolder.set(w);
+                        logger.error("connect ws[{}] succeed", url);
+                        webSocket = w;
                         w.closeHandler(closeHandler);
                         w.textMessageHandler(textMessageHandler);
+                        future.complete(null);
                     }).onFailure(t -> {
-                        logger.error("connect url[{}] failed", url, t);
-                        connect();
+                        logger.error("connect ws[{}] failed", url, t);
+                        connectInterval(future);
                     });
         }, waitTs, TimeUnit.MILLISECONDS);
     }
