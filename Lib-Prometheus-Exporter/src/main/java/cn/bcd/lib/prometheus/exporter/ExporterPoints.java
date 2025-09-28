e952768482b4ee1826917aedce6d398cf1d97b1e
baichangda <471267877@qq.com>
2025-09-28 15:34:34 +0800

    c

---
+++ Lib-Prometheus-Exporter/src/main/java/cn/bcd/lib/prometheus/exporter/ExporterPoints.java
@@ -0,0 +1,34 @@
+package cn.bcd.lib.prometheus.exporter;
+
+import cn.bcd.lib.base.exception.BaseException;
+import io.prometheus.metrics.core.metrics.Gauge;
+
+public class ExporterPoints {
+    public final Point[] points;
+    public final Gauge[] gauges;
+
+    public ExporterPoints(Point... points) {
+        this.points = points;
+        gauges = new Gauge[points.length];
+        for (int i = 0; i < points.length; i++) {
+            Point point = points[i];
+            gauges[i] = Gauge.builder()
+                    .name(point.name)
+                    .help(point.help)
+                    .register();
+        }
+    }
+
+    public void set(double... vals) {
+        if (vals.length != points.length) {
+            throw BaseException.get("points len[{}] not equal vals len[{}]", points.length, vals.length);
+        }
+        for (int i = 0; i < points.length; i++) {
+            gauges[i].set(vals[i]);
+        }
+    }
+
+    public record Point(String name, String help) {
+
+    }
+}
