188ff575242406329bbe770ff578af6bc26b745a
bcd <471267877@qq.com>
2026-07-23 09:17:07 +0800

    c

--- Lib-Parser-Protocol-GB32960/src/test/java/cn/bcd/lib/parser/protocol/gb32960/v2016/PacketReadBenchmark.java
+++ Lib-Parser-Protocol-GB32960/src/test/java/cn/bcd/lib/parser/protocol/gb32960/v2016/PacketReadBenchmark.java
@@ -23,7 +23,7 @@ import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode({Mode.Throughput})
 @OutputTimeUnit(TimeUnit.SECONDS)
-@Warmup(iterations = 3, time = 1)
+@Warmup(iterations = 5, time = 1)
 @Measurement(iterations = 5, time = 1)
 @Fork(1)
 @State(Scope.Thread)
