811bdc5f8dbcd0d7818dca4e54314770c23b6d26
bcd <471267877@qq.com>
2026-09-16 11:38:31 +0800

    c

--- Lib-Base/src/main/java/cn/bcd/lib/base/exception/BaseException.java
+++ Lib-Base/src/main/java/cn/bcd/lib/base/exception/BaseException.java
@@ -11,7 +11,7 @@ import java.lang.reflect.InvocationTargetException;
  * 建造此异常类的目的:
  * 1、在所有需要抛非运行时异常的地方,用此异常包装,避免方法调用时候需要捕获异常
  *    此时打印堆栈会打印完整的调用链、包括包装异常的调用链
- *    此时异常信息为null、如果需要获取真实异常信息、调用{@link #getRealExceptionMessage()}
+ *    此时异常信息为null、如果需要获取真实异常信息、调用{@link #getRealException()}
  * 2、在业务需要出异常的时候,定义异常并且抛出
  */
 public class BaseException extends RuntimeException {
@@ -43,8 +43,8 @@ public class BaseException extends RuntimeException {
         return target;
     }
 
-    public String getRealExceptionMessage(){
-        return ExceptionUtil.getRealException(this).getMessage();
+    public Throwable getRealException(){
+        return ExceptionUtil.getRealException(this);
     }
 
 
